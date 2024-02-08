9e706762b2b7af91bf12cd6ee0f4d0d25865a0e8
ORKEMEY <p.artem.p0@ukr.net>
2024-02-08 15:07:55 +0200

    sidebar udated

--- src/app/menu/sidebar/sidebar.component.ts
+++ src/app/menu/sidebar/sidebar.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 import { expandOnEnterAnimation, collapseOnLeaveAnimation } from 'angular-animations';
+import CredentialsService from '../../core/services/credentials.service';
 
 @Component({
   selector: 'sidebar',
@@ -10,10 +11,35 @@ import { expandOnEnterAnimation, collapseOnLeaveAnimation } from 'angular-animat
     collapseOnLeaveAnimation({ duration: 300 }),
   ],
 })
-export default class SidebarComponent {
+export default class SidebarComponent implements AfterViewInit {
   public isSidebarCollapsed: boolean = true;
 
   public isAccountSubMenuCollapsed: boolean = true;
+
+  public Login: string = this.credentialsService.getLogin().trim();
+
+  public get Logo(): string {
+    return this.Login?.[0] || '+';
+  }
+
+  public get Gratitude(): string {
+    if (this.Login) {
+      return `Hello, ${this.Login}`;
+    }
+    return 'Menu';
+  }
+
+  @ViewChild('gratitudeEl', { static: false })
+  public GratitudeEl: ElementRef | undefined;
+
+  public isGratitudeElOverflown: boolean = false;
+
+  constructor(private credentialsService: CredentialsService) {}
+
+  ngAfterViewInit(): void {
+    this.isGratitudeElOverflown =
+      this.GratitudeEl.nativeElement.scrollWidth > this.GratitudeEl.nativeElement.clientWidth;
+  }
 
   onCollapserClick() {
     if (!this.isSidebarCollapsed) {
